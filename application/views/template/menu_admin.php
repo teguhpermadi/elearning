c681124971895c6182ccfd5134a209b03d8903b1
teguhpermadi <tguh.permadi@gmail.com>
2020-11-25 23:47:30 +0700

    modif ui menu user add active class

--- application/views/template/menu_admin.php
+++ application/views/template/menu_admin.php
@@ -3,7 +3,7 @@
 		<!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
 		<li class="nav-item">
-			<a href="#" class="nav-link active">
+			<a href="<?= base_url('dashboard') ?>" class="nav-link <?php if($this->uri->segment(1) == '' || $this->uri->segment(1) == 'dashboard') { echo 'active';}?>">
 				<i class="nav-icon fas fa-tachometer-alt"></i>
 				<p>
 					Dashboard
@@ -11,7 +11,7 @@
 			</a>
 		</li>
 		<li class="nav-item">
-			<a href="<?= base_url('profil_sekolah') ?>" class="nav-link">
+			<a href="<?= base_url('profil_sekolah') ?>" class="nav-link <?php if($this->uri->segment(1) == 'profil_sekolah') { echo 'active';}?>">
 				<i class="nav-icon fas fa-school"></i>
 				<p>
 					Profil Sekolah
@@ -19,8 +19,8 @@
 			</a>
 		</li>
 		<li class="nav-header">Menu User</li>
-		<li class="nav-item">
-			<a href="#" class="nav-link">
+		<li class="nav-item <?php if($this->uri->segment(1) == 'profil_user') { echo 'active';}?>">
+			<a href="<?= base_url('profil_user') ?>" class="nav-link">
 				<i class="nav-icon fas fa-chalkboard-teacher"></i>
 				<p>
 					Profil User
@@ -28,7 +28,7 @@
 			</a>
 		</li>
 		<li class="nav-item">
-			<a href="<?= base_url('auth/logout') ?>" class="nav-link">
+			<a href="<?= base_url('auth/logout') ?>" class="nav-link <?php if($this->uri->segment(1) == 'auth/logout') { echo 'active';}?>">
 				<i class="nav-icon fas fa-sign-out-alt"></i>
 				<p>
 					Keluar
@@ -37,7 +37,7 @@
 		</li>
 		<li class="nav-header">Menu Admin</li>
 		<li class="nav-item">
-			<a href="#" class="nav-link">
+			<a href="<?= base_url('guru') ?>" class="nav-link <?php if($this->uri->segment(1) == 'guru') { echo 'active';}?>">
 				<i class="nav-icon fas fa-chalkboard-teacher"></i>
 				<p>
 					Guru
@@ -45,7 +45,7 @@
 			</a>
 		</li>
 		<li class="nav-item">
-			<a href="#" class="nav-link">
+			<a href="<?= base_url('siswa') ?>" class="nav-link <?php if($this->uri->segment(1) == 'siswa') { echo 'active';}?>">
 				<i class="nav-icon fas fa-user-graduate"></i>
 				<p>
 					Siswa
@@ -53,7 +53,7 @@
 			</a>
 		</li>
 		<li class="nav-item">
-			<a href="#" class="nav-link">
+			<a href="<?= base_url('kelas') ?>" class="nav-link <?php if($this->uri->segment(1) == 'kelas') { echo 'active';}?>">
 				<i class="nav-icon fas fa-book-reader"></i>
 				<p>
 					Kelas
@@ -61,7 +61,7 @@
 			</a>
 		</li>
 		<li class="nav-item">
-			<a href="#" class="nav-link">
+			<a href="<?= base_url('rombel') ?>" class="nav-link <?php if($this->uri->segment(1) == 'rombel') { echo 'active';}?>">
 				<i class="nav-icon fas fa-users"></i>
 				<p>
 					Rombel
@@ -69,7 +69,7 @@
 			</a>
 		</li>
 		<li class="nav-item">
-			<a href="#" class="nav-link">
+			<a href="<?= base_url('mapel') ?>" class="nav-link <?php if($this->uri->segment(1) == 'mapel') { echo 'active';}?>">
 				<i class="nav-icon fas fa-book"></i>
 				<p>
 					Mata Pelajaran
@@ -77,7 +77,7 @@
 			</a>
 		</li>
 		<li class="nav-item">
-			<a href="#" class="nav-link">
+			<a href="<?= base_url('pengajar') ?>" class="nav-link <?php if($this->uri->segment(1) == 'pengajar') { echo 'active';}?>">
 				<i class="nav-icon fas fa-book"></i>
 				<p>
 					Pengajar
