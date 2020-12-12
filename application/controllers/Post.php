fa27e58c24f00a66781372116d1fcaeab2e61888
teguhpermadi <tguh.permadi@gmail.com>
2020-12-12 08:26:05 +0700

    fix bug 12 desember

--- application/controllers/Post.php
+++ application/controllers/Post.php
@@ -225,4 +225,44 @@ class Post extends CI_Controller
         $this->load->view('post/view', $data);
         $this->load->view('template/footer');
     }
+
+    //Upload image summernote
+    function upload_image()
+    {
+        if (isset($_FILES["image"]["name"])) {
+            $config['upload_path'] = 'uploads/posts/images/';
+            $config['allowed_types'] = 'jpg|jpeg|png|gif';
+            $config['overwrite'] = TRUE;
+            $config['remove_spaces'] = TRUE;
+            $this->upload->initialize($config);
+            if (!$this->upload->do_upload('image')) {
+                $this->upload->display_errors();
+                return FALSE;
+            } else {
+                $data = $this->upload->data();
+                //Compress Image
+                $config['image_library'] = 'gd2';
+                $config['source_image'] = './uploads/posts/images/' . $data['file_name'];
+                $config['create_thumb'] = FALSE;
+                $config['maintain_ratio'] = TRUE;
+                $config['quality'] = '60%';
+                $config['width'] = 800;
+                $config['height'] = 800;
+                $config['new_image'] = './uploads/posts/images/' . $data['file_name'];
+                $this->load->library('image_lib', $config);
+                $this->image_lib->resize();
+                echo base_url() . 'uploads/posts/images/' . $data['file_name'];
+            }
+        }
+    }
+
+    //Delete image summernote
+    function delete_image()
+    {
+        $src = $this->input->post('src');
+        $file_name = str_replace(base_url(), '', $src);
+        if (unlink($file_name)) {
+            echo 'File Delete Successfully';
+        }
+    }
 }
--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -80,7 +80,7 @@
       var data = new FormData();
       data.append("image", image);
       $.ajax({
-        url: "<?php echo base_url('posts/upload_image') ?>",
+        url: "<?php echo base_url('post/upload_image') ?>",
         cache: false,
         contentType: false,
         processData: false,
@@ -101,7 +101,7 @@
           src: src
         },
         type: "POST",
-        url: "<?php echo base_url('posts/delete_image') ?>",
+        url: "<?php echo base_url('post/delete_image') ?>",
         cache: false,
         success: function(response) {
           console.log(response);
