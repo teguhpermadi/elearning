282980729258be127f53a14d2cac5ccc76625651
teguhpermadi <tguh.permadi@gmail.com>
2020-12-21 11:06:30 +0700

    balas komentar

--- application/views/post/view.php
+++ application/views/post/view.php
@@ -25,25 +25,24 @@
       <div class="card-body">
         <?= $post['content'] ?>
       </div>
+      <div class="card-footer">
+        <div class="default_replyrow">
+          <form method="POST" id="form_comment" class="mt-3 replyrow">
+            <div class="form-group">
+              <textarea name="content" id="content" class="form-control" placeholder="Tulis Komentar" rows="3" required></textarea>
+            </div>
+            <div class="form-group">
+              <input type="hidden" name="post_id" id="post_id" value="<?= $post['id'] ?>" />
+              <input type="hidden" name="author_id" id="author_id" value="<?= user_info()['id'] ?>" />
+              <input type="hidden" name="parrent_id" id="parrent_id" value="0" />
+              <input type="submit" name="submit" id="submit" class="btn btn-info" value="Kirim" />
+              <input type="button" name="cancel" id="cancel" class="btn btn-secondary" value="Batal">
+            </div>
+          </form>
+        </div>
+      </div>
       <div class="card-footer card-comments">
         <div id="display_comment"></div>
-      </div>
-      <hr>
-      <div class="card-footer">
-      <div class="default_replyrow">
-        <form method="POST" id="form_comment" class="mt-3 replyrow">
-          <div class="form-group">
-            <textarea name="content" id="content" class="form-control" placeholder="Tulis Komentar" rows="3" required></textarea>
-          </div>
-          <div class="form-group">
-            <input type="hidden" name="post_id" id="post_id" value="<?= $post['id'] ?>" />
-            <input type="hidden" name="author_id" id="author_id" value="<?= user_info()['id'] ?>" />
-            <input type="hidden" name="parrent_id" id="parrent_id" value="0" />
-            <input type="submit" name="submit" id="submit" class="btn btn-info" value="Kirim" />
-            <input type="button" name="cancel" id="cancel" class="btn btn-secondary" value="Batal">
-          </div>
-        </form>
-      </div>
       </div>
 
     </div>
