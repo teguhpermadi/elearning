a4ce24c4dec8bc10de329c1b25225764725bdd43
teguhpermadi <tguh.permadi@gmail.com>
2021-02-02 19:52:53 +0700

    edit all mapel and rekap absen siswa

--- application/views/materi/all_mapel.php
+++ application/views/materi/all_mapel.php
@@ -15,23 +15,45 @@
     <section class="content">
 
         <div class="row">
-            <?php foreach ($all_mapel as $mapel) { 
+            <?php foreach ($all_mapel as $mapel) {
                 $guru = $this->Materi_model->get_guru_by_mapel_kelas($mapel['id'], $kelas['id']);
                 $count_materi = $this->Materi_model->count_materi_by_guru_mapel_kelas($guru['id'], $mapel['id'], $kelas['id']);
                 $count_tugas = $this->Materi_model->count_tugas_by_guru_mapel_kelas($guru['id'], $mapel['id'], $kelas['id']);
-                ?>
-                
-                <div class="col-md-4">
-                    <div class="card">
+            ?>
+
+                <div class="col-md-6">
+                    <div class="card card-outline card-primary">
                         <div class="card-header">
                             <h3 class="card-title"><?= $mapel['nama'] ?></h3>
                         </div>
-                        <div class="card-body">
-                            <ul>
-                                <li>Pengajar: <?= $guru['first_name'] . ' ' .$guru['last_name'] ?> </li>
-                                <li>Materi: <?= $count_materi ?> postingan</li>
-                                <li>Tugas: <?= $count_tugas ?> postingan</li>
+                        <div class="card-body box-profile">
+                            <div class="text-center">
+                                
+                                <img class="profile-user-img img-fluid img-circle" src="" alt="User profile picture">
+                            </div>
+
+                            <h3 class="profile-username text-center">nama</h3>
+
+                            <ul class="list-group list-group-unbordered mb-3">
+                                <li class="list-group-item" href="#">
+                                    tes
+                                    <span class="float-right"><i class="fab fa-facebook"></i></span>
+                                </li>
+                                <li class="list-group-item">
+tes
+                                    <span class="float-right" href="#"><i class="fab fa-twitter"></i></span>
+                                </li>
+                                <li class="list-group-item">
+   tes
+                                    <span class="float-right" href="#"><i class="fab fa-instagram"></i></span>
+                                </li>
+                                <li class="list-group-item">
+tes
+                                    <span class="float-right" href="#"><i class="fab fa-youtube"></i></span>
+                                </li>
                             </ul>
+
+                            <!-- <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a> -->
                         </div>
                         <div class="card-footer">
                             <a href="<?= base_url('materi/view_mapel/') . $mapel['id'] . '-' . $kelas['id'] ?>" class="btn btn-primary">Lihat</a>
